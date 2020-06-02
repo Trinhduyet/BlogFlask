c9b99431f0ce0e13f2aa22d8e16ae1fb98341f42
Trinhduyet <trinh.ngo@vmodev.com>
2020-06-02 22:10:27 +0700

    initial commit

---
+++ flaskblog/users/routes.py
@@ -0,0 +1,73 @@
+
+from flask import render_template, url_for, flash, redirect, request, Blueprint
+from flask_login import login_user, current_user, logout_user, login_required
+from flaskblog import db, bcrypt
+from flaskblog.models import User, Post
+from flaskblog.users.forms import (RegistrationForm, LoginForm, ChangePasswordForm)
+from flaskblog.users.utils import save_picture
+users = Blueprint('users', __name__)
+
+
+@users.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data,
+                    email=form.email.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created', 'success')
+        return redirect(url_for('users.login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@users.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.password == form.password.data:
+            login_user(user)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('main.home'))
+        else:
+            flash('Login Unsuccessful. Please check email  and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@users.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
+
+
+@users.route("/change-pwd", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=current_user.username).first()
+        if user and form.password.data == form.re_password.data:
+            user.password = form.password.data
+            db.session.commit()
+            flash('Your account has been updated!', 'success')
+        else:
+            flash('Your password not match!', 'danger')
+        return redirect(url_for('users.account'))
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Change Password', image_file=image_file, form=form)
+
+
+@users.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=3)
+    return render_template('user_posts.html', posts=posts, user=user)
---
+++ run.py
@@ -0,0 +1,94 @@
+from flaskblog import create_app
+from flask_script import Manager
+
+from flaskblog.models import User, Post
+from flaskblog import db
+
+from datetime import datetime
+
+app = create_app()
+manager = Manager(app)
+
+@manager.command
+def init():
+    dropdb()
+    initdb()
+    filldb()
+
+@manager.command
+def initdb():
+    db.create_all()
+
+@manager.command
+def dropdb():
+    db.drop_all()
+
+@manager.command
+def filldb():
+    admin = User(
+        id = 1,
+        username = "admin",
+        email = "admin@gmail.com",
+        password = "111111"
+    )
+    db.session.add(admin)
+    db.session.commit()
+
+    user = User(
+        id = 2,
+        username = "user",
+        email = "user@gmail.com",
+        password = "123456"
+    )
+    db.session.add(user)
+    db.session.commit()
+
+    post = Post(
+        id = 1,
+        title = "Hello World",
+        date_posted = datetime(2019, 2, 28),
+        content = POST_1,
+        user_id = 1
+    )
+    db.session.add(post)
+    db.session.commit()
+
+    post = Post(
+        id=2,
+        title="Getting started",
+        date_posted=datetime(2019, 1, 19),
+        content=POST_2,
+        user_id=1
+    )
+    db.session.add(post)
+    db.session.commit()
+
+POST_1 = u"""
+First blog post.
+
+Nam quis urna est. Duis vel tincidunt quam. Vivamus odio tortor, suscipit vel
+pretium quis, imperdiet quis dolor. Integer molestie enim nec risus malesuada
+imperdiet. Donec pellentesque justo id sem tempor varius. Etiam ut tincidunt
+lorem. Nullam a tellus sem.
+Vestibulum a neque sed quam pharetra interdum. Quisque euismod dictum ipsum.
+Vivamus tincidunt mi at tellus pharetra placerat. Sed sed sem nisi, sit amet
+ultrices neque. Quisque eget turpis et sapien luctus auctor in ac magna.
+"""
+
+POST_2 = u"""
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel ipsum
+lectus. Pellentesque tempus enim sed leo imperdiet non lobortis nulla
+sollicitudin. Maecenas arcu orci, interdum eu rhoncus ut, blandit id felis.
+Mauris consectetur dui at felis ultricies tempus. Quisque molestie convallis
+lectus vitae viverra. Duis lobortis ultrices turpis, nec eleifend est
+venenatis nec. Sed sed lorem quis metus eleifend ullamcorper. Ut semper
+nulla a arcu ornare **condimentum**.
+
+Aliquam neque metus, posuere vitae condimentum ut, fermentum quis diam.
+*Nulla facilisi*. Proin sapien felis, tristique eu venenatis at,
+**accumsan** non dui. Vestibulum ante ipsum primis in faucibus orci luctus et
+ultrices posuere cubilia.
+"""
+
+if __name__ == "__main__":
+    manager.run()
