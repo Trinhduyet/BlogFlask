7a4ee81fc2b9bf86f4954a0efe360e92de8f6d14
Trinhduyet <trinh.ngo@vmodev.com>
2020-06-10 22:56:50 +0700

    ad code

--- run.py
+++ run.py
@@ -29,15 +29,15 @@ def filldb():
         id = 1,
         username = "admin",
         email = "admin@gmail.com",
-        password = "111111"
+        password = "123456"
     )
     db.session.add(admin)
     db.session.commit()
 
     user = User(
         id = 2,
-        username = "user",
-        email = "user@gmail.com",
+        username = "trinhduyet95",
+        email = "trinhduyet95@gmail.com",
         password = "123456"
     )
     db.session.add(user)
@@ -60,6 +60,14 @@ def filldb():
         content=POST_2,
         user_id=1
     )
+
+    post = Post(
+        id=3,
+        title="Welcome",
+        date_posted=datetime(2020, 6, 19),
+        content=POST_3,
+        user_id=1
+    )
     db.session.add(post)
     db.session.commit()
 
@@ -76,6 +84,8 @@ ultrices neque. Quisque eget turpis et sapien luctus auctor in ac magna.
 """
 
 POST_2 = u"""
+Test blog.
+
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel ipsum
 lectus. Pellentesque tempus enim sed leo imperdiet non lobortis nulla
 sollicitudin. Maecenas arcu orci, interdum eu rhoncus ut, blandit id felis.
@@ -88,6 +98,18 @@ Aliquam neque metus, posuere vitae condimentum ut, fermentum quis diam.
 *Nulla facilisi*. Proin sapien felis, tristique eu venenatis at,
 **accumsan** non dui. Vestibulum ante ipsum primis in faucibus orci luctus et
 ultrices posuere cubilia.
+"""
+
+POST_3 = u"""
+First blog post.
+
+Nam quis urna est. Duis vel tincidunt quam. Vivamus odio tortor, suscipit vel
+pretium quis, imperdiet quis dolor. Integer molestie enim nec risus malesuada
+imperdiet. Donec pellentesque justo id sem tempor varius. Etiam ut tincidunt
+lorem. Nullam a tellus sem.
+Vestibulum a neque sed quam pharetra interdum. Quisque euismod dictum ipsum.
+Vivamus tincidunt mi at tellus pharetra placerat. Sed sed sem nisi, sit amet
+ultrices neque. Quisque eget turpis et sapien luctus auctor in ac magna.
 """
 
 if __name__ == "__main__":
